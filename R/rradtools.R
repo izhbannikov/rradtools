9cda30e0261249be2f4a26b871aea275b5127147
izhbannikov <i.zhbannikov@gmail.com>
2013-12-02 07:32:59 +0400

    New version

--- R/rradtools.R
+++ R/rradtools.R
@@ -1,5 +1,5 @@
 #rradtools.R
-build_radsites <- function()#fnames,keys) 
+build_radsites <- function(fnames,keys) 
 {
    keys <- c("CCTGTG", "CTGCGA", "GAGGGA", "CATAGA", "GATCCA", "CGTTAA", "ACTGAT", "AACAAC", "GTACCG", "ATCGGG", "CATCAG", "AGTCAC", "GCACAC", "TCGTCA", "TCCACG", "TTCGAC", "ATAGTT", "GGTAAG", "GGGATT", "ACCTAA", "ACCAGT", "CTAGGC", "TCACGG", "TTCCCA")
    fnames <- c("~/bio/app/RAD/first4k.fastq")
